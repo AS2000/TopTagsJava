c8700b369f10ab699238797028d327d3c0116d0e
Anton <antonshepetko@gmail.com>
2020-03-12 17:09:04 +0200

    added gitignore

---
+++ src/main/java/lt/vianet/toptags/ToptagsApplication.java
@@ -0,0 +1,13 @@
+package lt.vianet.toptags;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToptagsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToptagsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lt/vianet/toptags/ToptagsApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.vianet.toptags;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToptagsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
